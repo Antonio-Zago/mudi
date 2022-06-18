9ba5b4e31a57f3099031fd78168d0cfeb2fef35c
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2022-06-18 19:00:03 -0300

    Integração com Spring Data

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,34 +1,26 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		List<Pedido> listaPedidos =  query.getResultList();
-		
-		
-		
+		List<Pedido> listaPedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", listaPedidos);
 		
 		
